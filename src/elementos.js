41b20731b124ecd6140dcb5b61a51c0b2c363902
ilir137 <appsilir@gmail.com>
2025-09-01 18:12:09 +0200

    Create elementos.js

---
+++ src/elementos.js
@@ -0,0 +1,8 @@
+// Se ponen en un archivo aparte porque si estuvieran en modelo.js el testing fallaría aunque no se esten usando en la funcion que se testea.
+
+export const spanPuntos = document.getElementById("puntaje");
+export const spanIntentos = document.getElementById("intentos");
+export const imgCarta = document.getElementById("imgCarta");
+export const divCartas = document.getElementById("misCartas");
+export const btnDameCarta = document.getElementById("btnDameCarta");
+export const btnMePlanto = document.getElementById("btnMePlanto");
